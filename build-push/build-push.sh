98f6da1983dc089fbd683fef65fa352192d0de13
Brent Graveland <brent@timescale.com>
2024-06-11 08:24:34 -0600

    Replace / with - in tag names
    This allows using github.ref_name in tags, since most of the time our
    branch names include slashes in them.

--- build-push/build-push.sh
+++ build-push/build-push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 for tag in ${tags}; do
+    # Search-and-replace all / characters to -, which allows using github.ref_name in the tag since most branches include a / character.
+    tag="${tag//\//-}"
+
     # If there is a target we use it
     echo "Building ${registry}:${tag}"
     [[ ${target} = '' ]] && docker build -t ${registry}:${tag} -f ${dockerfile} . || docker build -t ${registry}:${tag} --target ${target} -f ${dockerfile} .
